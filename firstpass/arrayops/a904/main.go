4ea1fe4cf6f87124eac8a04f980cb61bf8f41ada
peacelt <peacelt@163.com>
2025-04-03 19:05:05 +0800

    最小覆盖子串

--- firstpass/arrayops/a904/main.go
+++ firstpass/arrayops/a904/main.go
@@ -82,9 +82,14 @@ func totalFruit_error(fruits []int) int {
 	return maxNum
 }
 
-func totalFruit(fruits []int) (ans int) {
+// 我们可以使用滑动窗口解决本题，left 和 right 分别表示满足要求的窗口的左右边界，同时我们使用哈希表存储这个窗口内的数以及出现的次数。
+//
+// 我们每次将 right 移动一个位置，并将 fruits[right] 加入哈希表。如果此时哈希表不满足要求（即哈希表中出现超过两个键值对），那么我们需要不断移动 left，并将 fruits[left] 从哈希表中移除，直到哈希表满足要求为止。
+//
+// 需要注意的是，将 fruits[left] 从哈希表中移除后，如果 fruits[left] 在哈希表中的出现次数减少为 0，需要将对应的键值对从哈希表中移除
+func totalFruit(fruits []int) int {
 	cnt := map[int]int{}
-	left := 0
+	left, ans := 0, 0
 	for right, x := range fruits {
 		cnt[x]++
 		for len(cnt) > 2 {
@@ -95,14 +100,9 @@ func totalFruit(fruits []int) (ans int) {
 			}
 			left++
 		}
-		ans = max(ans, right-left+1)
+		if right-left+1 > ans {
+			ans = right - left + 1
+		}
 	}
-	return
-}
-
-func max(a, b int) int {
-	if b > a {
-		return b
-	}
-	return a
+	return ans
 }
